31e1fc8493c7de02ab6ca766ec4e85deba70db64
milodarling <milodarling98+othergithub@gmail.com>
2015-05-25 13:57:55 -0700

    Hi Vicki

--- src/ForGivens.java
+++ src/ForGivens.java
@@ -4,7 +4,7 @@ public class ForGivens {
 	public static void main(String[] args) {
 		System.out.println("Hi milo!");
 		System.out.println("Hi JJ!");
-		
+		System.out.println("Hi Vicki!");
 	}
 
 }
